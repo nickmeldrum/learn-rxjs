4e4aca526f2c42c8747c597255af211c8933079e
Nick Meldrum <nick@nickmeldrum.com>
2018-01-05 12:17:08 +0000

    filter

--- scan.test.js
+++ scan.test.js
@@ -1,9 +1,11 @@
 import { marbles } from 'rxjs-marbles'
 import 'rxjs/add/operator/scan'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/filter'
 
 const increment = obs => obs.map(x => x + 1)
 const sum = obs => obs.scan((acc, curr) => acc + curr)
+const onlySmallValues = obs => obs.filter(x => x <= 2)
 
 test(
   'map allows us to transform all elements of an observable',
@@ -29,5 +31,14 @@ test(
     const source = m.cold('a-b-c', { a: 1, b: 2, c: 3 })
     const expected = m.cold('a-b-c', { a: 2, b: 5, c: 9 })
     m.expect(sum(increment(source))).toBeObservable(expected)
+  }),
+)
+
+test(
+  'we can filter out rx elements',
+  marbles(m => {
+    const source = m.cold('a-b-c', { a: 1, b: 2, c: 3 })
+    const expected = m.cold('a-b--', { a: 1, b: 2 })
+    m.expect(onlySmallValues(source)).toBeObservable(expected)
   }),
 )
