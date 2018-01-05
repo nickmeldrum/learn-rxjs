68fe111114da20f12b530e6910433606188763bd
Nick Meldrum <nick@nickmeldrum.com>
2018-01-05 12:28:42 +0000

    reduce

--- scan.test.js
+++ scan.test.js
@@ -1,10 +1,13 @@
 import { marbles } from 'rxjs-marbles'
 import 'rxjs/add/operator/scan'
+import 'rxjs/add/operator/reduce'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/filter'
 
+const sum = (acc, curr) => acc + curr
 const increment = obs => obs.map(x => x + 1)
-const sum = obs => obs.scan((acc, curr) => acc + curr)
+const sumWithScan = obs => obs.scan(sum)
+const sumWithReduce = obs => obs.reduce(sum)
 const onlySmallValues = obs => obs.filter(x => x <= 2)
 
 test(
@@ -21,7 +24,7 @@ test(
   marbles(m => {
     const source = m.cold('a-b-c', { a: 1, b: 2, c: 3 })
     const expected = m.cold('a-b-c', { a: 1, b: 3, c: 6 })
-    m.expect(sum(source)).toBeObservable(expected)
+    m.expect(sumWithScan(source)).toBeObservable(expected)
   }),
 )
 
@@ -30,7 +33,7 @@ test(
   marbles(m => {
     const source = m.cold('a-b-c', { a: 1, b: 2, c: 3 })
     const expected = m.cold('a-b-c', { a: 2, b: 5, c: 9 })
-    m.expect(sum(increment(source))).toBeObservable(expected)
+    m.expect(sumWithScan(increment(source))).toBeObservable(expected)
   }),
 )
 
@@ -40,5 +43,14 @@ test(
     const source = m.cold('a-b-c', { a: 1, b: 2, c: 3 })
     const expected = m.cold('a-b--', { a: 1, b: 2 })
     m.expect(onlySmallValues(source)).toBeObservable(expected)
+  }),
+)
+
+test(
+  'the difference between scan and reduce: we use scan if we want intermediate values, reduce if we only want the final',
+  marbles(m => {
+    const source = m.cold('a-b-c-|', { a: 1, b: 2, c: 3 })
+    const expected = m.cold('------(a|)', { a: 6 })
+    m.expect(sumWithReduce(source)).toBeObservable(expected)
   }),
 )
